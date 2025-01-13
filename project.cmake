2f7db09bcb8ddd90490def589a235c07abb29afe
thomas-bc <thomas.boyerchammard@gmail.com>
2025-01-13 12:12:59 -0800

    Add tree structure and TestDeployment

--- project.cmake
+++ project.cmake
@@ -1,4 +1,6 @@
 # This CMake file is intended to register project-wide objects.
 # This allows for reuse between deployments, or other projects.
 
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/how-to")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/reference")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/tutorial")
